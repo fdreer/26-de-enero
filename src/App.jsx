dce8d1ff5bbc5ea58ed92fffdc0a991dd3957c3d
fdreer <francodreer@gmail.com>
2023-01-25 12:57:01 -0300

    Hook-state que no utilizaba eliminado

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import './App.css';
 import CountDown from './components/CountDown';
 
 function App() {
-  const [time, setTime] = useState(new Date().toLocaleTimeString());
-
   return (
     <main className="App">
       <CountDown />
